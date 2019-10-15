58bbee6c6f56fa74aff8673b869efb54db0a03a9
Raul <ruizvela271@gmail.com>
2019-10-15 15:22:20 +0200

    migrations y tabla pivot

--- Beauty/database/migrations/2014_10_12_000000_create_users_table.php
+++ Beauty/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('description');
+            $table->string('position');
+            $table->boolean('rol');
             $table->rememberToken();
             $table->timestamps();
         });
--- Beauty/database/migrations/2019_10_15_122013_user_group_relations_table.php
+++ Beauty/database/migrations/2019_10_15_122013_user_group_relations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOffersTable extends Migration
+class UserGroupRelationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreateOffersTable extends Migration
      */
     public function up()
     {
-        Schema::create('offers', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('title');
-            $table->string('description');
+        Schema::create('user_group_relations', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('group_id')->references('id')->on('groups');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ class CreateOffersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('offers');
+        Schema::dropIfExists('user_group_relations');
     }
 }
