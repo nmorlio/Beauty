4cddf683079c5ec506495345dd0df3afd52272cf
Raul <ruizvela271@gmail.com>
2019-10-16 12:10:40 +0200

    fixed campos nullables de migrations

--- Beauty/database/migrations/2014_10_12_000000_create_users_table.php
+++ Beauty/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,9 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('description');
-            $table->string('position');
-            $table->boolean('rol');
+            $table->string('description')->nullable();
+            $table->string('position')->nullable();
+            $table->boolean('rol')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
