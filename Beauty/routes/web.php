f76647bd50e1fc9d04657c14f91194ddd58c768d
Raul <ruizvela271@gmail.com>
2019-10-16 14:20:10 +0200

    prueba

--- Beauty/routes/web.php
+++ Beauty/routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes();sdfsdf
 
 //Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('/home', 'HomeController');
