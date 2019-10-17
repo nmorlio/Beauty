dac376234056ff8b69430050b14ccbec146f8e84
Raul <ruizvela271@gmail.com>
2019-10-17 10:15:49 +0200

    vista profile

--- Beauty/app/Http/Controllers/ProfileController.php
+++ Beauty/app/Http/Controllers/ProfileController.php
@@ -8,9 +8,6 @@ class ProfileController extends Controller
 {
     public function show(User $user)
     {
-       echo "Nombre: $user->name<br>";
-       echo "Email: $user->email<br>";
-       echo "Descripción: $user->description<br>";
-       echo "Puesto: $user->position<br>";
+       return view('profile',['user'=>$user]);
     }
 }
---
+++ Beauty/resources/views/profile.blade.php
@@ -0,0 +1,5 @@
+<?php
+echo "Nombre: $user->name<br>";
+echo "Email: $user->email<br>";
+echo "Descripción: $user->description<br>";
+echo "Puesto: $user->position<br>";
