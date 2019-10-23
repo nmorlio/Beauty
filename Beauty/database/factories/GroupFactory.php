7cdeceab019bd45ef3457c2c5709ee933a2557ed
Raul <ruizvela271@gmail.com>
2019-10-23 12:36:20 +0200

    Seeder y Factory de Groups

--- Beauty/database/factories/GroupFactory.php
+++ Beauty/database/factories/GroupFactory.php
@@ -7,6 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Group::class, function (Faker $faker) {
     return [
-        //
+        'name'=> $faker->name,
+        'description'=> $faker->name,
     ];
 });
