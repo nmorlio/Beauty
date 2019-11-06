a8996009390cfea6262a8fb4de696d760818d4fe
Raul <ruizvela271@gmail.com>
2019-11-06 12:57:40 +0100

    Boton editar comentarios en comentarios/list.blade

--- Beauty/resources/views/comments/list.blade.php
+++ Beauty/resources/views/comments/list.blade.php
@@ -2,4 +2,7 @@
     <li> {{$comment->user_id->name}}: {{$comment->body}}
     </li>
     <br>
+    <form action="/comment/{{$comment->id}}/edit" method="GET">
+        <input type="submit" value="Editar">
+    </form>
 @endforeach
